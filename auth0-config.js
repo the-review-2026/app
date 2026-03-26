ed5287c59b57700fa2b9809af6ddb7c71cd17105
Naoki Kikuta <naoki.kikuta0712@gmail.com>
2026-03-26 19:19:45 +0900

    Update files

--- auth0-config.js
+++ auth0-config.js
@@ -1,6 +1,6 @@
 window.AUTH0_CONFIG = {
-  domain: "YOUR_AUTH0_DOMAIN",
-  clientId: "YOUR_AUTH0_CLIENT_ID",
+  domain: "the-review.jp.auth0.com",
+  clientId: "sREm6xFL7Sx57uPbBwLR6MrZfuL64A34",
   audience: "",
   scope: "openid profile email",
   redirectUri: window.location.origin + window.location.pathname,
